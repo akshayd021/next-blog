2b07b11378edb3b066f56ee06ee38d3fc75c0a78
akshayd021 <akshaydholakiya69@gmail.com>
2024-07-04 19:51:51 +0530

    front-end

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import { CardList, CategoryList, Featured, Menu } from "@/components";
 
-export default function Home() {
-  return <div>Hello</div>;
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={page} />
+        <Menu />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Featured from "./featured/Featured";
+import CategoryList from "./categoryList/CategoryList";
+import Menu from "./menu/Menu";
+import CardList from "./cardList/CardList";
+import Navbar from "./navbar/Navbar";
+import Footer from "./footer/Footer";
+
+export {Featured, CategoryList, CardList,Menu , Navbar , Footer}
